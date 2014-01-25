3e603c901a0241b902ea85679b5d97209db84f8f
Philip Stears <philip@driveworks.co.uk>
2014-01-25 13:23:50 +0000

    Read package json

--- src/core/package.js
+++ src/core/package.js
@@ -1,6 +1,13 @@
-function Package() {
+var fs = require("fs");
+
+function Package(packagePath) {
+	this.load(packagePath);
 }
 
 var $ = Package.prototype;
+
+$.load = function load(packagePath) {
+	this.__data = require(fs.realpathSync(packagePath));
+};
 
 module.exports = Package;
