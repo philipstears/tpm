c01805f1df1f83064eb656d8fabfb948cbbcb0da
Philip Stears <philip@driveworks.co.uk>
2014-01-25 20:09:45 -0600

    Question/answer driver

--- src/cli/initVerb.js
+++ src/cli/initVerb.js
@@ -1,6 +1,7 @@
 var path = require("path");
 var fs = require("fs");
 var readline = require("readline");
+var util = require("util");
 
 module.exports = function(args) {
 	return {
@@ -14,13 +15,44 @@ module.exports = function(args) {
 				name: path.basename(process.cwd())
 			};
 
-			rl.question("name (" + baseDescription.name + "): ", function(answer) {
-				if (answer !== '') {
-					baseDescription.name = answer;
-				}
+			var questions = [
+				["name (%s)", "name"],
+			];
 
+			var previousQuestion = -1;
+
+			function nextQuestion() {
+				if (previousQuestion === questions.length - 1) {
+					done();
+				} else {
+					var currentQuestion = ++previousQuestion;
+					askQuestion(questions[currentQuestion]);
+				}
+			}
+
+			function done() {
+				console.log("tspackage.json file:\n");
+				console.log(JSON.stringify(baseDescription, "\t"));
 				rl.close();
-			});
+			}
+
+			function askQuestion(question) {
+				var prompt = question[0] + ": ";
+				var field = question[1];
+				var fieldValue = baseDescription[field];
+
+				prompt = util.format(prompt, fieldValue);
+
+				rl.question(prompt, function(answer) {
+					if (answer !== '') {
+						baseDescription[field] = answer;
+					}
+
+					nextQuestion();
+				});
+			}
+
+			nextQuestion();
 		}
 	}
 };
