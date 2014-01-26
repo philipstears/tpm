08c27bd310edaf0cfa52a758992f4e54b2b414de
Philip Stears <philip@driveworks.co.uk>
2014-01-25 19:52:21 -0600

    Basic setup for using readline

--- src/cli/initVerb.js
+++ src/cli/initVerb.js
@@ -1,6 +1,26 @@
+var path = require("path");
+var fs = require("fs");
+var readline = require("readline");
+
 module.exports = function(args) {
 	return {
 		exec: function exec() {
+			var rl = readline.createInterface({
+				input: process.stdin,
+				output: process.stdout
+			});
+
+			var baseDescription = {
+				name: path.basename(process.cwd())
+			};
+
+			rl.question("name (" + baseDescription.name + "): ", function(answer) {
+				if (answer !== '') {
+					baseDescription.name = answer;
+				}
+
+				rl.close();
+			});
 		}
 	}
 };
